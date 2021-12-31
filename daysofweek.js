5c0f981d6e2482c1419d228193d9258e2557c452
Lotanna1989 <78662437+Lotanna1989@users.noreply.github.com>
2021-12-31 16:58:52 +0000

    Add files via upload

---
+++ daysofweek.js
@@ -0,0 +1,27 @@
+let day = '5'
+
+switch(day){
+    case '1' :
+        console.log("Today is Sunday");
+        break;
+    case '2':
+        console.log("Today is Monday");
+        break;
+    case '3':
+        console.log("Today is Tuesday");
+        break;
+    case '4':
+        console.log("Today is Wednesday");
+        break;
+    case '5':
+        console.log("Today is Thursday");
+        break;
+    case '6':
+        console.log("Today is Friday");
+        break;
+    case '7':
+        console.log("Today is Saturday");
+        break;
+}
+
+console.log('days of the week')
